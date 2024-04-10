c91005ee6fc37166debbf79e2dff8748c76989d6
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2024-04-10 15:51:51 +0100

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -14,7 +14,7 @@ class FileStorage:
             return FileStorage.__objects
         else:
             return {key: obj for key, obj in FileStorage.__objects.items()
-                        if isinstance(obj, cls)}
+                    if isinstance(obj, cls)}
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
