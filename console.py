d9692c24ade3a8c259d3ea88323181086144c90c
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2024-04-10 12:58:50 +0100

    Update console.py

--- console.py
+++ console.py
@@ -114,7 +114,7 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, args):
-    """ Create an object of any class"""
+        """ Create an object of any class"""
         if not args:
             print("** class name missing **")
             return
