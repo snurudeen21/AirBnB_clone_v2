33821c903b50bc6698746b89522943ca2776ab0d
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2024-04-11 06:06:47 +0100

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """This is the user class"""
-from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
