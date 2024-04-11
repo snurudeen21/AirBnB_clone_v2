15b2159d4f6d541bf56ddc8cc855ed2be02b1f5b
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2024-04-11 06:10:07 +0100

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """This is the user class"""
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
@@ -11,7 +12,7 @@ class User(BaseModel, Base):
     """This is the class for user
     Attributes:
         email: email address
-        password: password for your login
+        password: password for you login
         first_name: first name
         last_name: last name
     """
