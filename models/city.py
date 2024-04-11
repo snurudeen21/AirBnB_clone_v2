bc74c8872c58bb9825051927cff8c0433f7595a3
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2024-04-11 06:01:07 +0100

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """This is the city class"""
-from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy import ForeignKey
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from models.place import Place
 
